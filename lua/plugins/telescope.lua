3abfbbec7df906d8be693e10c9710d1444a497b8
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2023-08-22 10:59:08 +0200

    chore: cleanup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,7 @@ local function keymap()
   end, { desc = '[/] Fuzzily search in current buffer' })
 
   vim.keymap.set('n', '<leader>f', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-  vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>F', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
   vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
   vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
   vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
