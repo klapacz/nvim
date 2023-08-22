6bdd56c65ad2049eecee1f26198131f9c095eb61
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2023-08-23 01:41:22 +0200

    refactor: telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,21 +1,10 @@
-local function git_status()
-  local builtin = require('telescope.builtin')
-  local previewers = require("telescope.previewers")
-  local delta = previewers.new_termopen_previewer {
-    get_command = function(entry)
-      return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value }
-    end
-  }
-  builtin.git_status({ previewer = delta })
-end
-
 local function keymap()
   local builtin = require('telescope.builtin')
 
  -- See `:help telescope.builtin`
   vim.keymap.set('n', '<leader>?', builtin.oldfiles, { desc = '[?] Find recently opened files' })
-  vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = '[ ] Find existing buffers' })
-  vim.keymap.set("n", "<leader>B", git_status)
+  vim.keymap.set("n", "<leader>b", builtin.git_status)
+  vim.keymap.set('n', '<leader>B', builtin.buffers, { desc = '[ ] Find existing buffers' })
   vim.keymap.set('n', '<leader>/', function()
     -- You can pass additional configuration to telescope to change theme, layout, etc.
     builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -40,13 +29,8 @@ return {
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
