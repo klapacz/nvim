bf20eb9c56788e6313e87166525bdfb8497b00ea
Korneliusz Łapacz <lapacz.kornel@gmail.com>
2023-11-24 17:27:49 +0100

    chore: disable char motions in flash.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,15 +3,21 @@ return {
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		---@type Flash.Config
-		opts = {},
+		opts = {
+			modes = {
+				-- options used when flash is activated through
+				-- `f`, `F`, `t`, `T`, `;` and `,` motions
+				char = { enabled = false },
+			},
+		},
 		-- stylua: ignore
 		keys = {
 			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
 			-- TODO: keymaps do not work
-			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+			-- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			-- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			-- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 		},
 	},
 
